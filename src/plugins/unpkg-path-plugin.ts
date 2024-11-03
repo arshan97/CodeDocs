2241de339e4ddbd3580bee64754a9f2e2e126e20
Arshan <arshan97@github.com>
2024-11-03 20:24:57 +0530

    fix typescipt error

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -48,7 +48,9 @@ export const unpkgPathPlugin = () => {
         }
 
         //caching
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
         if (cachedResult) {
           return cachedResult;
@@ -56,7 +58,7 @@ export const unpkgPathPlugin = () => {
 
         const { data, request } = await axios.get(args.path);
 
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
