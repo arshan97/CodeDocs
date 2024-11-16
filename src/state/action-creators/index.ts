17b5921043f29279788f8ef70be5569e6911ead9
Arshan <arshanshaikh97@gmail.com>
2024-11-17 01:22:18 +0530

    action creators setup

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,11 @@
+import {
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+} from "../actions";
+
+export const updateCell = (): UpdateCellAction => {};
+export const deleteCell = (): DeleteCellAction => {};
+export const moveCell = (): MoveCellAction => {};
+export const insertCellBefore = (): InsertCellBeforeAction => {};
