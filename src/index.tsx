4ce4a9383f278d179915d9abea61dec5ffc17f1d
Arshan <arshanshaikh97@gmail.com>
2024-11-17 01:16:31 +0530

    react redux connection

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import ReactDOM from "react-dom/client";
 import "bulmaswatch/slate/bulmaswatch.min.css";
 import CodeCell from "./components/CodeCell";
 import TextEditor from "./components/TextEditor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div style={{ padding: "20px" }}>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div style={{ padding: "20px" }}>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
