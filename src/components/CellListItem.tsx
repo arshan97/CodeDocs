d234739eeb532957087804dd8c90909fca9e3b4a
Arshan <arshanshaikh97@gmail.com>
2024-11-18 01:10:54 +0530

    Added ActionBar Component

--- src/components/CellListItem.tsx
+++ src/components/CellListItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Cell } from "../state";
 import CodeCell from "./CodeCell";
 import TextEditor from "./TextEditor";
+import ActionBar from "./ActionBar";
 
 interface CellListItemProps {
   cell: Cell;
@@ -16,7 +17,12 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     child = <TextEditor cell={cell} />;
   }
 
-  return <div>{child}</div>;
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
 };
 
 export default CellListItem;
