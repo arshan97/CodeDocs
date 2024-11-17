1d3c961f727f49f32f71494a242f1c1b7b6e5a37
Arshan <arshanshaikh97@gmail.com>
2024-11-18 02:49:54 +0530

    styling of AddCell Component

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./CellListItem";
+import AddCell from "./AddCell";
 
 const CellList = () => {
   const cells = useTypedSelector(({ cells }) => {
@@ -10,10 +11,20 @@ const CellList = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <div className={cells.length === 0 ? "force-visible" : ""}>
+        <AddCell nextCellId={null} />
+      </div>
+    </div>
+  );
 };
 
 export default CellList;
